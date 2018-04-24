535bb7f9869057ff6033d77fb91d309e194525db
Chenyang Meng <sunnym412@gmail.com>
2018-04-24 02:37:36 -0500

    SPI function

--- HW4/hw4.X/SPI1.C
+++ HW4/hw4.X/SPI1.C
@@ -1,37 +1,43 @@
 #include"SPI1.h"
 
-// send a byte via spi and return the response
-unsigned char SPI1_IO(unsigned char ch) {
-    SPI1BUF = ch;
-    while(!SPI1STATbits.SPIRBF) {;} // wait to receive the byte
-    return SPI1BUF;
+// initialize SPI1
+void initSPI1(void) {
+  // Master Device: SPI1(PIC32)
+  // Pins: SDO1(A1), SCK1(B14), SDI1(B8), SS1(A0)
+  // Manually control the SS1 as digital output
+  RPA1Rbits.RPA1R = 0b0011;   // set SDO1 to A1
+  SDI1Rbits.SDI1R = 0b0100;   // set SDI1 to B8, in this homework SDI1 is not connected to SPI DAC
+  SPI1CON = 0;                // turn off the SPI module and reset it
+  SPI1BUF;                    // clear the rx buffer by reading from it
+  SPI1BRG = 0x1;              // baud rate to 12.5MHz SPIxBRG = Fpb/(2*Fsck) - 1 = 1, Fpb = 50MHz, Fsck = 12.5MHz
+  SPI1STATbits.SPIROV = 0     // clear the overflow bit
+  SPI1CONbits.CKP = 0;        // the clock is idle when low, active when high
+  SPI1CONbits.CKE = 1;        // data changes when clock goes from hi to lo (since CKP is 0)
+  SPI1CONbits.MSTEN = 1;      // master operation
+  SPI1CONbits.ON = 1;         // turn on SPI1
+  TRISAbits.TRISA0 = 0;       // set A0 as an output
 }
 
-// initialize SPI1
-void SPI1_init() {
-    // Master - SPI1, pins are: SDI1(B8), SDO1(A1), SCK1(B14).  
-    // we manually control SS1 as a digital output (A0)
-    RPA1Rbits.RPA1R = 0b0011; // Set SDO1 to A1
-    SDI1Rbits.SDI1R = 0b0100; // Set SDI1 to B8, actually not connected to DAC
-    SPI1CON = 0;              // turn off the SPI module and reset it
-    SPI1BUF;                  // clear the rx buffer by reading from it
-    SPI1BRG = 0x1;            // SPI1BRG = (80MHz/(2*20MHz))-1 = 1
-    SPI1STATbits.SPIROV = 0;  // clear the overflow bit
-    SPI1CONbits.CKE = 1;      // data changes when clock goes from HIGH to LOW (since CKP is 0)
-    SPI1CONbits.MSTEN = 1;    // master operation
-    SPI1CONbits.ON = 1;       // turn on SPI1
-    TRISAbits.TRISA0 = 0;
+// send a byte via SPI and return the response
+char SPI1_IO(char write) {
+  SPI1BUF = write;
+  while(!SPI1STATbits.SPIRBF) { ; } // wait until the master receives the data
+  return SPI1BUF;
 }
-//where channel is 0 or 1 (for VoutA and VoutB), and voltage is the 8-bit output level.
-void setVoltage(char channel, unsigned short voltage){
-    unsigned char LSB,MSB;
-    
-    LSB = (voltage << 2)&0x00FF;               
-    MSB = channel << 7;
-    MSB |= 0b01110000;
-    MSB |= voltage >> 6;  
-    CS = 0;                        // enable SPI1 by set chip select line low
-    SPI1_IO(MSB);
-    SPI1_IO(LSB);      
-    CS = 1;                        // raise the chip select line, ending communication
+
+// where channel is 0 or 1 (for VoutA and VoutB), and voltage is the 10-bit output level.
+void setVoltage(char channel, unsigned char voltage) {
+  unsigned char LSB, MSB;
+
+  MSB =  channel << 7;
+  MSB |= 0b01110000;
+  MSB |= voltage >> 6;
+
+  LSB = (voltage << 2) & 0b11111111; // 0xFF
+
+  CS = 0;                     // activate SPI1 by setting the chip line low
+  SPI1_IO(MSB);
+  SPI1_IO(LSB);
+  CS = 1;                     // deactivate the SPI1 by rasing the line to high, ending the communication
+  
 }
