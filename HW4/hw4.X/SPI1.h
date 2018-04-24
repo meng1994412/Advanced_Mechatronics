5a8cdddb7e07f216a973fbfa81834dce030ba78f
Chenyang Meng <sunnym412@gmail.com>
2018-04-24 02:42:51 -0500

    SPI1 Lib

--- HW4/hw4.X/SPI1.h
+++ HW4/hw4.X/SPI1.h
@@ -5,12 +5,12 @@
 #include <sys/attribs.h>  // __ISR macro
 
 #define CS LATAbits.LATA0 // chip select pin
-#define CHANNELA 0         
+#define CHANNELA 0
 #define CHANNELB 1
 #define PI 3.1415926535898
 
-unsigned char SPI1_IO(unsigned char ch); // send a byte via spi and return the response
 void SPI1_init(); // initialize SPI1
+unsigned char SPI1_IO(unsigned char ch); // send a byte via spi and return the response
 void setVoltage(char channel, unsigned short voltage); // set channel and voltage
-    
+
 #endif
