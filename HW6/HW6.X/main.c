198bf7b423937dc1bdbfc128c61a58b8b9c728a4
Chenyang Meng <sunnym412@gmail.com>
2018-04-25 23:16:27 -0500

    init code

--- HW6/HW6.X/main.c
+++ HW6/HW6.X/main.c
@@ -1,149 +1,84 @@
-/* ************************************************************************** */
-/** Descriptive File Name
+#include<xc.h>           // processor SFR definitions
+#include<sys/attribs.h>  // __ISR macro
 
-  @Company
-    Company Name
+// DEVCFG0
+#pragma config DEBUG = OFF // no debugging
+#pragma config JTAGEN = OFF // no jtag
+#pragma config ICESEL = ICS_PGx1 // use PGED1 and PGEC1
+#pragma config PWP = OFF // no write protect
+#pragma config BWP = OFF // no boot write protect
+#pragma config CP = OFF // no code protect
 
-  @File Name
-    filename.c
+// DEVCFG1
+#pragma config FNOSC = PRIPLL // use primary oscillator with pll
+#pragma config FSOSCEN = OFF // turn off secondary oscillator
+#pragma config IESO = OFF // no switching clocks
+#pragma config POSCMOD = HS // high speed crystal mode
+#pragma config OSCIOFNC = OFF // disable secondary osc
+#pragma config FPBDIV = DIV_1 // divide sysclk freq by 1 for peripheral bus clock
+#pragma config FCKSM = CSDCMD // do not enable clock switch
+#pragma config WDTPS = PS1048576 // use slowest wdt    //???
+#pragma config WINDIS = OFF // wdt no window mode
+#pragma config FWDTEN = OFF // wdt disabled
+#pragma config FWDTWINSZ = WINSZ_25 // wdt window at 25%
 
-  @Summary
-    Brief description of the file.
+// DEVCFG2 - get the sysclk clock to 48MHz from the 8MHz crystal
+#pragma config FPLLIDIV = DIV_2 // divide input clock to be in range 4-5MHz
+#pragma config FPLLMUL = MUL_24 // multiply clock after FPLLIDIV
+#pragma config FPLLODIV = DIV_2 // divide clock after FPLLMUL to get 48MHz
+#pragma config UPLLIDIV = DIV_2 // divider for the 8MHz input clock, then multiplied by 12 to get 48MHz for USB
+#pragma config UPLLEN = ON // USB clock on
 
-  @Description
-    Describe the purpose of this file.
- */
-/* ************************************************************************** */
-
-/* ************************************************************************** */
-/* ************************************************************************** */
-/* Section: Included Files                                                    */
-/* ************************************************************************** */
-/* ************************************************************************** */
-
-/* This section lists the other files that are included in this file.
- */
-
-/* TODO:  Include other files here if needed. */
-
-
-/* ************************************************************************** */
-/* ************************************************************************** */
-/* Section: File Scope or Global Data                                         */
-/* ************************************************************************** */
-/* ************************************************************************** */
-
-/*  A brief description of a section can be given directly below the section
-    banner.
- */
-
-/* ************************************************************************** */
-/** Descriptive Data Item Name
-
-  @Summary
-    Brief one-line summary of the data item.
-    
-  @Description
-    Full description, explaining the purpose and usage of data item.
-    <p>
-    Additional description in consecutive paragraphs separated by HTML 
-    paragraph breaks, as necessary.
-    <p>
-    Type "JavaDoc" in the "How Do I?" IDE toolbar for more information on tags.
-    
-  @Remarks
-    Any additional remarks
- */
-int global_data;
+// DEVCFG3
+#pragma config USERID = 0 // some 16bit userid, doesn't matter what
+#pragma config PMDL1WAY = OFF // allow multiple reconfigurations
+#pragma config IOL1WAY = OFF // allow multiple reconfigurations
+#pragma config FUSBIDIO = ON // USB pins controlled by USB module
+#pragma config FVBUSONIO = ON // USB BUSON controlled by USB module
 
 
-/* ************************************************************************** */
-/* ************************************************************************** */
-// Section: Local Functions                                                   */
-/* ************************************************************************** */
-/* ************************************************************************** */
+int main() {
 
-/*  A brief description of a section can be given directly below the section
-    banner.
- */
+    __builtin_disable_interrupts();
 
-/* ************************************************************************** */
+    // set the CP0 CONFIG register to indicate that kseg0 is cacheable (0x3)
+    __builtin_mtc0(_CP0_CONFIG, _CP0_CONFIG_SELECT, 0xa4210583);
 
-/** 
-  @Function
-    int ExampleLocalFunctionName ( int param1, int param2 ) 
+    // 0 data RAM access wait states
+    BMXCONbits.BMXWSDRM = 0x0;
 
-  @Summary
-    Brief one-line description of the function.
+    // enable multi vector interrupts
+    INTCONbits.MVEC = 0x1;
 
-  @Description
-    Full description, explaining the purpose and usage of the function.
-    <p>
-    Additional description in consecutive paragraphs separated by HTML 
-    paragraph breaks, as necessary.
-    <p>
-    Type "JavaDoc" in the "How Do I?" IDE toolbar for more information on tags.
+    // disable JTAG to get pins back
+    DDPCONbits.JTAGEN = 0;
 
-  @Precondition
-    List and describe any required preconditions. If there are no preconditions,
-    enter "None."
+    // LED pin as output
+    TRISAbits.TRISA4 = 0;
 
-  @Parameters
-    @param param1 Describe the first parameter to the function.
-    
-    @param param2 Describe the second parameter to the function.
+    // Pushbutton pin as input
+    TRISBbits.TRISB4 = 1;
 
-  @Returns
-    List (if feasible) and describe the return values of the function.
-    <ul>
-      <li>1   Indicates an error occurred
-      <li>0   Indicates an error did not occur
-    </ul>
+    // Turn off LED
+    LATAbits.LATA4 = 0;
 
-  @Remarks
-    Describe any special behavior not described above.
-    <p>
-    Any additional remarks.
+    SPI1_init();
+    LCD_init();
 
-  @Example
-    @code
-    if(ExampleFunctionName(1, 2) == 0)
-    {
-        return 3;
+
+    __builtin_enable_interrupts();
+
+    while(1) {
+	     // use _CP0_SET_COUNT(0) and _CP0_GET_COUNT() to test the PIC timing
+       // remember the core timer runs at half the sysclk
+       _CP0_SET_COUNT(0);
+
+       while (_CP0_GET_COUNT() < 12000) { // to obtain 1kHz LED
+         while (!PORTBbits.RB4) {
+           LATAbits.LATA4 = 0; // pin RB4 is Pushbutton, if Pushbutton is pushed, LED goes to low
+         }
+       }
+       LATAINV = 0x10; // flip bits 0 and 1
+       //LATAbits.LATA4 = !LATAbits.LATA4;
     }
- */
-static int ExampleLocalFunction(int param1, int param2) {
-    return 0;
 }
-
-
-/* ************************************************************************** */
-/* ************************************************************************** */
-// Section: Interface Functions                                               */
-/* ************************************************************************** */
-/* ************************************************************************** */
-
-/*  A brief description of a section can be given directly below the section
-    banner.
- */
-
-// *****************************************************************************
-
-/** 
-  @Function
-    int ExampleInterfaceFunctionName ( int param1, int param2 ) 
-
-  @Summary
-    Brief one-line description of the function.
-
-  @Remarks
-    Refer to the example_file.h interface header for function usage details.
- */
-int ExampleInterfaceFunction(int param1, int param2) {
-    return 0;
-}
-
-
-/* *****************************************************************************
- End of File
- */
