b0b885ec146252d4a9d83391c525805b5fed5c21
Chenyang Meng <sunnym412@gmail.com>
2018-05-10 01:43:20 -0500

    read serial port and get data from IMU

--- HW10/firmware/src/read_serial.py
+++ HW10/firmware/src/read_serial.py
@@ -1,8 +1,54 @@
 import serial
 import matplotlib.pyplot as plt
 
-ser = serial.Serial('COM4', 9600)
-print(ser.name)
+def main():
+    # open cmd mode to find out the port name
+    ser = serial.Serial('COM4', 9600)
 
-s = ser.read(100)
-print(s)
+    # write letter 'r' to PIC in order to recieve the data
+    ser.write('r')
+
+    # each data list
+    num = []
+    raw = []
+    MAF = []
+    IIR = []
+    FIR = []
+
+
+    for i in range(100):
+        line = ser.readline()
+        print(line)
+        data = line.split(', ')
+
+        # separate the data point
+        raw.append(data[1])
+        MAF.append(data[2])
+        IIR.append(data[3])
+        FIR.append(data[4])
+
+    # change data type from string to float number
+    # print(raw)
+    raw = map(float, raw)
+    print(raw)
+    # print(MAF)
+    MAF = map(float, MAF)
+    print(MAF)
+    # print(IIR)
+    IIR = map(float, IIR)
+    print(IIR)
+    # print(FIR)
+    FIR = map(float, FIR)
+    print(FIR)
+
+    # plot the data
+    plt.plot(raw, label='raw')
+    plt.plot(MAF, label='MAF')
+    plt.plot(IIR, label='IIR')
+    plt.plot(FIR, label='FIR')
+    plt.legend()
+    plt.show()
+
+
+
+if __name__ == '__main__': main()
