9274fb00f67d7d901a749f7e1de6ba642ca4aeeb
Chenyang Meng <sunnym412@gmail.com>
2018-06-11 15:07:36 -0500

    PI control done

---
+++ HW16/PIC32Program/firmware/src/app.c
@@ -0,0 +1,495 @@
+// *****************************************************************************
+// *****************************************************************************
+// Section: Included Files
+// *****************************************************************************
+// *****************************************************************************
+
+#include "app.h"
+#include <stdio.h>
+#include <xc.h>
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: Global Data Definitions
+// *****************************************************************************
+// *****************************************************************************
+
+uint8_t APP_MAKE_BUFFER_DMA_READY dataOut[APP_READ_BUFFER_SIZE];
+uint8_t APP_MAKE_BUFFER_DMA_READY readBuffer[APP_READ_BUFFER_SIZE];
+int len, i = 0;
+int startTime = 0;
+char rx[64];        // the raw data
+int rxPos = 0;      // how much data has been stored
+int gotRx = 0;      // the flag
+int rxVal = 0;      // a place to store the int that was received
+int rxVal1 = 0;
+int get_center_mass_high(void){
+
+    return rxVal;
+}
+
+int get_center_mass_low(void){
+
+    return rxVal1;
+}
+
+int get_diffierence(){
+    int result = rxVal - rxVal1;
+    return result;
+}
+APP_DATA appData;
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: Application Callback Functions
+// *****************************************************************************
+// *****************************************************************************
+
+/*******************************************************
+ * USB CDC Device Events - Application Event Handler
+ *******************************************************/
+
+USB_DEVICE_CDC_EVENT_RESPONSE APP_USBDeviceCDCEventHandler
+(
+        USB_DEVICE_CDC_INDEX index,
+        USB_DEVICE_CDC_EVENT event,
+        void * pData,
+        uintptr_t userData
+        ) {
+    APP_DATA * appDataObject;
+    appDataObject = (APP_DATA *) userData;
+    USB_CDC_CONTROL_LINE_STATE * controlLineStateData;
+
+    switch (event) {
+        case USB_DEVICE_CDC_EVENT_GET_LINE_CODING:
+
+            /* This means the host wants to know the current line
+             * coding. This is a control transfer request. Use the
+             * USB_DEVICE_ControlSend() function to send the data to
+             * host.  */
+
+            USB_DEVICE_ControlSend(appDataObject->deviceHandle,
+                    &appDataObject->getLineCodingData, sizeof (USB_CDC_LINE_CODING));
+
+            break;
+
+        case USB_DEVICE_CDC_EVENT_SET_LINE_CODING:
+
+            /* This means the host wants to set the line coding.
+             * This is a control transfer request. Use the
+             * USB_DEVICE_ControlReceive() function to receive the
+             * data from the host */
+
+            USB_DEVICE_ControlReceive(appDataObject->deviceHandle,
+                    &appDataObject->setLineCodingData, sizeof (USB_CDC_LINE_CODING));
+
+            break;
+
+        case USB_DEVICE_CDC_EVENT_SET_CONTROL_LINE_STATE:
+
+            /* This means the host is setting the control line state.
+             * Read the control line state. We will accept this request
+             * for now. */
+
+            controlLineStateData = (USB_CDC_CONTROL_LINE_STATE *) pData;
+            appDataObject->controlLineStateData.dtr = controlLineStateData->dtr;
+            appDataObject->controlLineStateData.carrier = controlLineStateData->carrier;
+
+            USB_DEVICE_ControlStatus(appDataObject->deviceHandle, USB_DEVICE_CONTROL_STATUS_OK);
+
+            break;
+
+        case USB_DEVICE_CDC_EVENT_SEND_BREAK:
+
+            /* This means that the host is requesting that a break of the
+             * specified duration be sent. Read the break duration */
+
+            appDataObject->breakData = ((USB_DEVICE_CDC_EVENT_DATA_SEND_BREAK *) pData)->breakDuration;
+
+            /* Complete the control transfer by sending a ZLP  */
+            USB_DEVICE_ControlStatus(appDataObject->deviceHandle, USB_DEVICE_CONTROL_STATUS_OK);
+
+            break;
+
+        case USB_DEVICE_CDC_EVENT_READ_COMPLETE:
+
+            /* This means that the host has sent some data*/
+            appDataObject->isReadComplete = true;
+            break;
+
+        case USB_DEVICE_CDC_EVENT_CONTROL_TRANSFER_DATA_RECEIVED:
+
+            /* The data stage of the last control transfer is
+             * complete. For now we accept all the data */
+
+            USB_DEVICE_ControlStatus(appDataObject->deviceHandle, USB_DEVICE_CONTROL_STATUS_OK);
+            break;
+
+        case USB_DEVICE_CDC_EVENT_CONTROL_TRANSFER_DATA_SENT:
+
+            /* This means the GET LINE CODING function data is valid. We dont
+             * do much with this data in this demo. */
+            break;
+
+        case USB_DEVICE_CDC_EVENT_WRITE_COMPLETE:
+
+            /* This means that the data write got completed. We can schedule
+             * the next read. */
+
+            appDataObject->isWriteComplete = true;
+            break;
+
+        default:
+            break;
+    }
+
+    return USB_DEVICE_CDC_EVENT_RESPONSE_NONE;
+}
+
+/***********************************************
+ * Application USB Device Layer Event Handler.
+ ***********************************************/
+void APP_USBDeviceEventHandler(USB_DEVICE_EVENT event, void * eventData, uintptr_t context) {
+    USB_DEVICE_EVENT_DATA_CONFIGURED *configuredEventData;
+
+    switch (event) {
+        case USB_DEVICE_EVENT_SOF:
+
+            /* This event is used for switch debounce. This flag is reset
+             * by the switch process routine. */
+            appData.sofEventHasOccurred = true;
+            break;
+
+        case USB_DEVICE_EVENT_RESET:
+
+            /* Update LED to show reset state */
+
+            appData.isConfigured = false;
+
+            break;
+
+        case USB_DEVICE_EVENT_CONFIGURED:
+
+            /* Check the configuration. We only support configuration 1 */
+            configuredEventData = (USB_DEVICE_EVENT_DATA_CONFIGURED*) eventData;
+            if (configuredEventData->configurationValue == 1) {
+                /* Update LED to show configured state */
+
+                /* Register the CDC Device application event handler here.
+                 * Note how the appData object pointer is passed as the
+                 * user data */
+
+                USB_DEVICE_CDC_EventHandlerSet(USB_DEVICE_CDC_INDEX_0, APP_USBDeviceCDCEventHandler, (uintptr_t) & appData);
+
+                /* Mark that the device is now configured */
+                appData.isConfigured = true;
+
+            }
+            break;
+
+        case USB_DEVICE_EVENT_POWER_DETECTED:
+
+            /* VBUS was detected. We can attach the device */
+            USB_DEVICE_Attach(appData.deviceHandle);
+            break;
+
+        case USB_DEVICE_EVENT_POWER_REMOVED:
+
+            /* VBUS is not available any more. Detach the device. */
+            USB_DEVICE_Detach(appData.deviceHandle);
+            break;
+
+        case USB_DEVICE_EVENT_SUSPENDED:
+
+            /* Switch LED to show suspended state */
+            break;
+
+        case USB_DEVICE_EVENT_RESUMED:
+        case USB_DEVICE_EVENT_ERROR:
+        default:
+            break;
+    }
+}
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: Application Local Functions
+// *****************************************************************************
+// *****************************************************************************
+
+/*****************************************************
+ * This function is called in every step of the
+ * application state machine.
+ *****************************************************/
+
+bool APP_StateReset(void) {
+    /* This function returns true if the device
+     * was reset  */
+
+    bool retVal;
+
+    if (appData.isConfigured == false) {
+        appData.state = APP_STATE_WAIT_FOR_CONFIGURATION;
+        appData.readTransferHandle = USB_DEVICE_CDC_TRANSFER_HANDLE_INVALID;
+        appData.writeTransferHandle = USB_DEVICE_CDC_TRANSFER_HANDLE_INVALID;
+        appData.isReadComplete = true;
+        appData.isWriteComplete = true;
+        retVal = true;
+    } else {
+        retVal = false;
+    }
+
+    return (retVal);
+}
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: Application Initialization and State Machine Functions
+// *****************************************************************************
+// *****************************************************************************
+
+void APP_Initialize(void) {
+    /* Place the App state machine in its initial state. */
+    appData.state = APP_STATE_INIT;
+
+    /* Device Layer Handle  */
+    appData.deviceHandle = USB_DEVICE_HANDLE_INVALID;
+
+    /* Device configured status */
+    appData.isConfigured = false;
+
+    /* Initial get line coding state */
+    appData.getLineCodingData.dwDTERate = 9600;
+    appData.getLineCodingData.bParityType = 0;
+    appData.getLineCodingData.bParityType = 0;
+    appData.getLineCodingData.bDataBits = 8;
+
+    /* Read Transfer Handle */
+    appData.readTransferHandle = USB_DEVICE_CDC_TRANSFER_HANDLE_INVALID;
+
+    /* Write Transfer Handle */
+    appData.writeTransferHandle = USB_DEVICE_CDC_TRANSFER_HANDLE_INVALID;
+
+    /* Initialize the read complete flag */
+    appData.isReadComplete = true;
+
+    /*Initialize the write complete flag*/
+    appData.isWriteComplete = true;
+
+    /* Reset other flags */
+    appData.sofEventHasOccurred = false;
+    //appData.isSwitchPressed = false;
+
+    /* Set up the read buffer */
+    appData.readBuffer = &readBuffer[0];
+
+    //setup output compare
+    RPA0Rbits.RPA0R = 0b0101; // A0 to OC1
+    RPB13Rbits.RPB13R = 0b0101; // B13 to OC4
+    T2CONbits.TCKPS = 0; // Timer2 prescaler N=1 (1:1)
+    PR2 = 2399; // 48000000 Hz / 20000 Hz / 1 - 1 = 2399 (20kHz PWM from 48MHz clock with 1:1 prescaler)
+    TMR2 = 0; // initial TMR2 count is 0
+    OC1CONbits.OCM = 0b110; // PWM mode without fault pin; other OCxCON bits are defaults
+    OC1RS = 0; // duty cycle
+    OC1R = 0; // initialize before turning OC1 on; afterward it is read-only
+    OC4CONbits.OCM = 0b110; // PWM mode without fault pin; other OCxCON bits are defaults
+    OC4RS = 0; // duty cycle
+    OC4R = 0; // initialize before turning OC4 on; afterward it is read-only
+    T2CONbits.ON = 1; // turn on Timer2
+    OC1CONbits.ON = 1; // turn on OC1
+    OC4CONbits.ON = 1; // turn on OC4
+
+    // external pulses count
+    T5CKR = 0b0100; // B9 is read by T5CK
+    T3CKR = 0b0100; // B8 is read by T3CK
+
+    T5CONbits.TCS = 1; // count external pulses
+    PR5 = 0xFFFF; // enable counting to max value of 2^16 - 1
+    TMR5 = 0; // set the timer count to zero
+    T5CONbits.ON = 1; // turn Timer on and start counting
+    T3CONbits.TCS = 1; // count external pulses
+    PR3 = 0xFFFF; // enable counting to max value of 2^16 - 1
+    TMR3 = 0; // set the timer count to zero
+    T3CONbits.ON = 1; // turn Timer on and start counting
+
+    //interrupt on timer 4
+    T4CONbits.TCKPS = 2; // Timer4 prescaler N=4
+    PR4 = 23999; // 48000000 Hz / 500 Hz / 4 - 1 = 23999 (500Hz from 48MHz clock with 4:1 prescaler)
+    TMR4 = 0; // initial TMR4 count is 0
+    T4CONbits.ON = 1;
+    IPC4bits.T4IP = 4; // priority for Timer 4
+    IFS0bits.T4IF = 0; // clear interrupt flag for Timer4
+    IEC0bits.T4IE = 1; // enable interrupt for Timer4
+}
+
+
+void APP_Tasks(void) {
+    /* Update the application state machine based
+     * on the current state */
+
+    switch (appData.state) {
+        case APP_STATE_INIT:
+
+            /* Open the device layer */
+            appData.deviceHandle = USB_DEVICE_Open(USB_DEVICE_INDEX_0, DRV_IO_INTENT_READWRITE);
+
+            if (appData.deviceHandle != USB_DEVICE_HANDLE_INVALID) {
+                /* Register a callback with device layer to get event notification (for end point 0) */
+                USB_DEVICE_EventHandlerSet(appData.deviceHandle, APP_USBDeviceEventHandler, 0);
+
+                appData.state = APP_STATE_WAIT_FOR_CONFIGURATION;
+            } else {
+                /* The Device Layer is not ready to be opened. We should try
+                 * again later. */
+            }
+
+            break;
+
+        case APP_STATE_WAIT_FOR_CONFIGURATION:
+
+            /* Check if the device was configured */
+            if (appData.isConfigured) {
+                /* If the device is configured then lets start reading */
+                appData.state = APP_STATE_SCHEDULE_READ;
+            }
+            break;
+
+        case APP_STATE_SCHEDULE_READ:
+
+
+
+            if (APP_StateReset()) {
+                break;
+            }
+
+            /* If a read is complete, then schedule a read
+             * else wait for the current read to complete */
+
+            appData.state = APP_STATE_WAIT_FOR_READ_COMPLETE;
+            if (appData.isReadComplete == true) {
+                appData.isReadComplete = false;
+                appData.readTransferHandle = USB_DEVICE_CDC_TRANSFER_HANDLE_INVALID;
+
+                USB_DEVICE_CDC_Read(USB_DEVICE_CDC_INDEX_0,
+                        &appData.readTransferHandle, appData.readBuffer,
+                        APP_READ_BUFFER_SIZE);
+
+                int ii = 0;
+                // loop thru the characters in the buffer
+                while (appData.readBuffer[ii] != 0) {
+                    // if you got a newline
+                    if (appData.readBuffer[ii] == '\n' || appData.readBuffer[ii] == '\r') {
+                        rx[rxPos] = 0; // end the array
+                        sscanf(rx, "%d,%d", &rxVal, &rxVal1);
+                        gotRx = 1; // set the flag
+                        break; // get out of the while loop
+                    } else if (appData.readBuffer[ii] == 0) {
+                        break; // there was no newline, get out of the while loop
+                    } else {
+                        // save the character into the array
+                        rx[rxPos] = appData.readBuffer[ii];
+                        rxPos++;
+                        ii++;
+                    }
+                }
+
+                if (appData.readTransferHandle == USB_DEVICE_CDC_TRANSFER_HANDLE_INVALID) {
+                    appData.state = APP_STATE_ERROR;
+                    break;
+                }
+            }
+
+            break;
+
+        case APP_STATE_WAIT_FOR_READ_COMPLETE:
+        case APP_STATE_CHECK_TIMER:
+
+
+            if (APP_StateReset()) {
+                break;
+            }
+
+            /* Check if a character was received or a switch was pressed.
+             * The isReadComplete flag gets updated in the CDC event handler. */
+
+//            if (gotRx || _CP0_GET_COUNT() - startTime > (48000000 / 2 / 150)) {
+//                appData.state = APP_STATE_SCHEDULE_WRITE;
+//            }
+
+            appData.state = APP_STATE_SCHEDULE_WRITE;
+/*
+            if (appData.isReadComplete || _CP0_GET_COUNT() - startTime > (48000000 / 2 / 5)) {
+                appData.state = APP_STATE_SCHEDULE_WRITE;
+            }
+*/
+            break;
+
+
+        case APP_STATE_SCHEDULE_WRITE:
+
+            if (APP_StateReset()) {
+                break;
+            }
+
+            /* Setup the write */
+
+            appData.writeTransferHandle = USB_DEVICE_CDC_TRANSFER_HANDLE_INVALID;
+            appData.isWriteComplete = false;
+            appData.state = APP_STATE_WAIT_FOR_WRITE_COMPLETE;
+
+            if (gotRx) {
+                len = sprintf(dataOut, "got: %d\r\n", rxVal);
+                i++;
+                USB_DEVICE_CDC_Write(USB_DEVICE_CDC_INDEX_0,
+                        &appData.writeTransferHandle,
+                        dataOut, len,
+                        USB_DEVICE_CDC_TRANSFER_FLAGS_DATA_COMPLETE);
+                rxPos = 0;
+                gotRx = 0;
+            } else {
+                len = sprintf(dataOut, "%d\r\n", i);
+                i++;
+                USB_DEVICE_CDC_Write(USB_DEVICE_CDC_INDEX_0,
+                        &appData.writeTransferHandle, dataOut, len,
+                        USB_DEVICE_CDC_TRANSFER_FLAGS_DATA_COMPLETE);
+                startTime = _CP0_GET_COUNT();
+            }
+
+/*
+            len = sprintf(dataOut, "%d\r\n", i);
+            i++;
+            if (appData.isReadComplete) {
+                USB_DEVICE_CDC_Write(USB_DEVICE_CDC_INDEX_0,
+                        &appData.writeTransferHandle,
+                        appData.readBuffer, 1,
+                        USB_DEVICE_CDC_TRANSFER_FLAGS_DATA_COMPLETE);
+            } else {
+                USB_DEVICE_CDC_Write(USB_DEVICE_CDC_INDEX_0,
+                        &appData.writeTransferHandle, dataOut, len,
+                        USB_DEVICE_CDC_TRANSFER_FLAGS_DATA_COMPLETE);
+                startTime = _CP0_GET_COUNT();
+            }
+*/
+            break;
+
+        case APP_STATE_WAIT_FOR_WRITE_COMPLETE:
+
+            if (APP_StateReset()) {
+                break;
+            }
+
+            /* Check if a character was sent. The isWriteComplete
+             * flag gets updated in the CDC event handler */
+
+            if (appData.isWriteComplete == true) {
+                appData.state = APP_STATE_SCHEDULE_READ;
+            }
+
+            break;
+
+        case APP_STATE_ERROR:
+            break;
+        default:
+            break;
+    }
+}
---
+++ HW16/PIC32Program/firmware/src/system_config/default/system_interrupt.c
@@ -0,0 +1,82 @@
+// *****************************************************************************
+// *****************************************************************************
+// Section: Included Files
+// *****************************************************************************
+// *****************************************************************************
+
+#include "system/common/sys_common.h"
+#include "app.h"
+#include "system_definitions.h"
+
+int speed_error = 0, error= 0;
+int left=0, right=0;
+int MAX_DUTY = 2400;
+
+int center_mass_high = 0;
+int center_mass_low = 0;
+int center_mass_low_old = 0;
+
+void __ISR(_USB_1_VECTOR, ipl4AUTO) _IntHandlerUSBInstance0(void)
+{
+
+    DRV_USBFS_Tasks_ISR(sysObj.drvUSBObject);
+}
+
+
+void __ISR(_TIMER_4_VECTOR, IPL4SOFT) Timer4ISR(void) {
+    // code for PI control goes here
+    center_mass_high = get_center_mass_high();
+    center_mass_low = get_center_mass_low();
+    speed_error = get_difference();
+    float kp = 2.6;
+
+
+    if (-15 <= speed_error && speed_error <= 15) {
+        MAX_DUTY = 1660;
+    } else if (speed_error < -15) {
+        speed_error = -speed_error;
+        MAX_DUTY = 1900 - speed_error * 5;
+        if(MAX_DUTY < 1200){
+            MAX_DUTY = 1200;
+        }
+    } else {
+        MAX_DUTY = 1900 - speed_error * 5;
+        if(MAX_DUTY < 1200){
+            MAX_DUTY = 1200;
+        }
+    }
+
+
+    if (center_mass_low == 0) {
+        MAX_DUTY = 1510;
+        center_mass_low = center_mass_low_old;
+        kp = 6.0;
+    }
+
+
+    error = center_mass_low - 319; // 240 means the dot is in the middle of the screen
+    if(error < -30) {              // slow down the left motor to steer to the left
+        error  = -error;
+        left = 0.85 * MAX_DUTY - kp * error;
+        right = 0.85 * MAX_DUTY - error;
+        if (left < 0) {
+            left = 0;
+        }
+    }
+    else if(error > 30) {           // slow down the right motor to steer to the right
+        right = 0.85 * MAX_DUTY - kp * error;
+        left = 0.85 * MAX_DUTY - error;
+        if (right < 0) {
+            right = 0;
+        }
+    } else {
+     right = MAX_DUTY;
+     left = MAX_DUTY;
+    }
+    OC1RS = right;
+    OC4RS = left;
+
+    center_mass_low_old = center_mass_low;
+
+    IFS0bits.T4IF = 0;              // clear interrupt flag, last line
+}
